2034ed9c8d9e2628d5cad4d37acbab79ef10c188
Victor Lucas <victorlucas@alu.ufc.br>
2020-06-27 18:42:13 -0300

    Example - use of HasRoles

--- src/routes/UserRoute.js
+++ src/routes/UserRoute.js
@@ -1,5 +1,5 @@
 import { UserController } from "../controller";
-import { RetrieveFilters, CheckToken } from "../middleware";
+import { RetrieveFilters, CheckToken, HasRoles } from "../middleware";
 
 export default class UserRoute {
 
@@ -7,7 +7,7 @@ export default class UserRoute {
         this.controller = new UserController()
     }
 
-    #registerRoutes(app){
+    useRoute(app){
         /* 
         * A ArrowFunction é necessária pra que os métodos não percam a instância do
         * Controller quando passarem os parâmetros. Caso não seja passado dessa forma
@@ -15,17 +15,13 @@ export default class UserRoute {
         * estar disponíveis, gerando um erro de ponteiro vazio (this ficará = undefined).
         */
 
-        app.post('/login', (req,res) => this.controller.login(req,res));
-        app.get('/', [RetrieveFilters, CheckToken], (req,res) => this.controller.listUsers(req,res));
-        app.post('/', [CheckToken], (req,res) => this.controller.saveUser(req,res));
-        app.put('/:id', [CheckToken], (req,res) => this.controller.updateUser(req,res));
-        app.delete('/:id', [CheckToken], (req,res) => this.controller.deleteUser(req,res));
+       app.post('/user/login', (req,res) => this.controller.login(req,res));
+       app.get('/user/', [CheckToken, RetrieveFilters, HasRoles(["READ_USERS"])], (req,res) => this.controller.listUsers(req,res));
+       app.post('/user/', [CheckToken], (req,res) => this.controller.saveUser(req,res));
+       app.put('/user/:id', [CheckToken], (req,res) => this.controller.updateUser(req,res));
+       app.delete('/user/:id', [CheckToken], (req,res) => this.controller.deleteUser(req,res));
 
-        return app;
-    }
-
-    useRoute(app){
-        app.use('/user', this.#registerRoutes(app))
+       return app;
     }
 
 
